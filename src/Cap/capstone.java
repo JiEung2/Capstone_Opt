3b35f47ee63c62c845762352287c1c3a849148bd
JiEung2 <https://github.com/JiEung2>
2023-05-29 16:16:17 +0900

    사용된 차량 대수 변경

--- src/Cap/capstone.java
+++ src/Cap/capstone.java
@@ -13,18 +13,17 @@ public class capstone {
 	
 	public static void model1() throws IOException{
 		GetData.main(null);
-		int n = GetData.n;
-		int K = GetData.K+5;
-		int Q = GetData.Q;
+		int n = GetData.n; //노드 수
+		int K = GetData.K; //차량의 수
+		int Q = GetData.Q; //차량 용량
 		
 		double[][] c = new double [n][n]; //비용
 		int[] p = new int[n]; //pickup 개수
 		int[] d = new int[n]; //delivery 개수
-		//int[] s = new int[n]; //추가 서비스시간
 		double[][][] std = new double[n][n][K]; //표준편차
-		double[][][] v = new double[n][n][K]; //속력
-		double[][] D = new double[n][n]; //거리
-		double s[] = new double[n];
+		double[][][] v = new double[n][n][K]; //노드간 차량의 속력
+		double[][] D = new double[n][n]; //각 노드간 거리
+		double s[] = new double[n]; //pickup, delivery 시간
 		
 		D = GetData.d;
 		d = GetData.delivery;
@@ -39,18 +38,11 @@ public class capstone {
 			}
 		}
 		
-
-		double[][] cost = new double[n][n];
-		for(int i=0; i<n;i++) {
-			for(int j=0; j<n; j++) {
-				cost[i][j] = (D[i][j]*10)/(GetData.speed[i][j]/60.*1000);
-			}
-		}
 		for(int i=0; i<n;i++) {
 			for(int j=0; j<n; j++) {
 				if(i==j)
 					c[i][j] = 0;
-				else c[i][j] = cost[i][j];
+				else c[i][j] = (D[i][j]*10)/(GetData.speed[i][j]/60.*1000);
 			}
 		}
 		
@@ -59,23 +51,23 @@ public class capstone {
 //				c[i][j] = D[i][j];
 //			}
 //		}
+		
 		double PI = 1.2816;
-
-		//double a =0.1;
-		double a,b, e;
+		double a,b,e;
 		a = 1.5;
 		b = 1/3;
 		e = 0.5;
 		
-		double[] B = new double[K];
-		for(int k=0; k<K; k++) {
-			B[k] = 300;
-		}
-		//30 -> 해가 바뀌면서 솔루션 나옴
-		
 		for(int i=0; i<n; i++) {
 			s[i] = (a+e)*d[i] + b*p[i];
 		}
+		
+		double[] B = new double[K];
+		for(int k=0; k<K; k++) {
+			B[k] = 360;
+		}
+		
+		
 		
 		String fileName = "Result K="+K+" PI="+PI+" B[k]="+(int)B[0]+" Q="+(int)Q;
 		String filePath = "/Users/hijieung/Desktop/Opt_Lab/Result/";
@@ -89,7 +81,7 @@ public class capstone {
 			IloNumVar[][] z = new IloNumVar[n][];
 			IloNumVar[] w = new IloNumVar[K];
 			
-			
+			//비음 제약식
 			for(int i = 0; i<n; i++) {
 				for(int j = 0; j<n; j++) {
 					x[i][j] = cplex.numVarArray(K, 0, 1, IloNumVarType.Int);
@@ -102,7 +94,8 @@ public class capstone {
 			for(int i=0; i<K; i++) {
 				w[i] = cplex.numVar(0, Double.MAX_VALUE);
 			}
-			int sum=0;
+			
+			
 			//목적함수
 			IloLinearNumExpr objective = cplex.linearNumExpr();
 			for(int i=0; i<n; i++) {
@@ -113,7 +106,6 @@ public class capstone {
 						
 					}
 				}
-				//sum+=s[i];
 			}
 			
 			cplex.addMinimize(objective);
@@ -121,7 +113,7 @@ public class capstone {
 			
 			//제약식
 			
-			//con1
+			//제약식(1)
 			IloLinearNumExpr[] con1 = new IloLinearNumExpr[n];
 			for(int i=1; i<n; i++) {
 				con1[i] = cplex.linearNumExpr();
@@ -134,7 +126,7 @@ public class capstone {
 				
 			}
 			
-			//con2
+			//제약식(2)
 			IloLinearNumExpr[][] con2 = new IloLinearNumExpr[n][K];
 			for(int i=0; i<n; i++) {
 				for(int k=0; k<K; k++) {
@@ -148,7 +140,7 @@ public class capstone {
 			}
 
 			
-			//con3
+			//제약식(3)
 			IloLinearNumExpr[] con3 = new IloLinearNumExpr[K];
 			for(int k=0; k<K; k++) {
 				con3[k] = cplex.linearNumExpr();
@@ -158,7 +150,7 @@ public class capstone {
 				cplex.addLe(con3[k], 1);
 			}
 			
-			//con4
+			//제약식(4)
 			IloLinearNumExpr[][] con4 = new IloLinearNumExpr[n][n];
 			for(int i=0; i<n; i++) {
 				for(int j=0; j<n; j++) {
@@ -171,7 +163,7 @@ public class capstone {
 				}
 			}
 			
-			//con5
+			//제약식(5)
 			IloLinearNumExpr[] con5 = new IloLinearNumExpr[n];
 			for(int i=1; i<n; i++) {
 				con5[i] = cplex.linearNumExpr();
@@ -182,7 +174,7 @@ public class capstone {
 				cplex.addEq(con5[i], p[i]);
 			}
 			
-			//con6
+			//제약식(6)
 			IloLinearNumExpr[] con6 = new IloLinearNumExpr[n];
 			for(int i=1; i<n; i++) {
 				con6[i] = cplex.linearNumExpr();
@@ -193,7 +185,7 @@ public class capstone {
 				cplex.addEq(con6[i], d[i]);
 			}
 			
-			//con7
+			//제약식(7-1)
 			IloNumExpr[] con7 = new IloNumExpr[K];
 			for(int k=0; k<K; k++) {
 				con7[k] = cplex.quadNumExpr();
@@ -207,7 +199,7 @@ public class capstone {
 				cplex.addLe(con7[k],0);
 			}
 			
-			//con8, con9
+			//제약식(7-2)
 			IloLinearNumExpr[] con8 = new IloLinearNumExpr[K];
 			IloLinearNumExpr[] con9 = new IloLinearNumExpr[K];
 			for(int k=0; k<K; k++) {
@@ -222,12 +214,13 @@ public class capstone {
 				cplex.addLe(cplex.sum(con8[k], cplex.prod(PI, w[k])), cplex.sum(B[k], cplex.prod(-1,con9[k])));
 			}
 			
-			//con10
+			//제약식(8)
 			for(int i=0; i<n; i++) {
 				for(int k=0; k<K; k++) {
 					cplex.addEq(x[i][i][k], 0);
 				}
 			}
+			
 			cplex.setParam(IloCplex.DoubleParam.TimeLimit, 3600);
 			
 			long start = System.currentTimeMillis();
@@ -237,15 +230,6 @@ public class capstone {
 			double time = (end - start) / 1000.0;
 			System.out.println("obj = "+obj);
 			System.out.println("총 얼마? = "+cplex.getObjValue()*9500/60);
-//			for(int k=0; k<K; k++) {
-//				for (int i = 0; i < n; i++) {
-//					for (int j = 0; j < n; j++) {
-//						if (i != j && cplex.getValue(x[i][j][k]) != 0) {
-//							System.out.println((k+1) +"번째 차량: i = " + i + ", j = " + j);
-//						}
-//		            }
-//		        }
-//			}
 			BufferedWriter fw = new BufferedWriter(new FileWriter(filePath+fileName+fileType, true));
 			
 			fw.write(Double.toString(obj));
@@ -260,7 +244,7 @@ public class capstone {
 			fw.newLine();
 			double TotalTime = 0;
 			int knum = 0;
-			int tmpk = 0;
+			int tmpk = -1;
 			for (int k = 0; k < K; k++) {
 	            int i = 0;
 	            int j = 0;
